457f7dde95a91c676f0dc6385f978bdeb6a4e53b
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-08-07 23:51:35 +0300

    add main golang source file

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"gopkg.in/src-d/go-git.v4"
+)
+
+func main() {
+	repoULR := os.Args[1]
+	fmt.Println("Git repo URL:", repoULR)
+
+	repoPath := "./repo-data/" + strings.Split(repoULR, "/")[1]
+	if !strings.Contains(repoULR, "@") {
+		repoPath = "./repo-data/" + strings.Split(repoULR, "/")[4]
+	}
+	fmt.Println("Saved in ", repoPath)
+
+	repoPathDotGit := repoPath + "/.git"
+	// If repo exists, git pull
+	if _, err := os.Stat(repoPathDotGit); !os.IsNotExist(err) {
+		// We instanciate a new repository targeting the given path (the .git folder)
+		repo, err := git.PlainOpen(repoPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Get working directory for the repository
+		worktree, err := repo.Worktree()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Pull the latest changes from the origin remote and merge into the current branch
+		err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			dt := time.Now()
+			fmt.Println(dt.Format("01-02-2006 15:04:05"), "pull success")
+		}
+	} else {
+		// This case is for repo not existing, so we git clone
+		_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
+			URL:      repoULR,
+			Progress: os.Stdout,
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			dt := time.Now()
+			fmt.Println(dt.Format("01-02-2006 15:04:05"), "clone success")
+		}
+	}
+}
