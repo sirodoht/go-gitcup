e42e1418f6702c428fd0d205a48c3e838e2f4a54
Theodore Keloglou <theodorekeloglou@gmail.com>
2019-08-08 01:41:47 +0300

    add support for repos defined in a file

--- main.go
+++ main.go
@@ -1,24 +1,62 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	"gopkg.in/src-d/go-git.v4"
 )
 
 func main() {
-	repoULR := os.Args[1]
-	fmt.Println("Git repo URL:", repoULR)
+	filePtr := flag.String("f", "-", "File with list of repo URL to backup")
+	flag.Parse()
 
-	repoPath := "./repo-data/" + strings.Split(repoULR, "/")[1]
-	if !strings.Contains(repoULR, "@") {
-		repoPath = "./repo-data/" + strings.Split(repoULR, "/")[4]
+	if *filePtr != "-" {
+		readFromFile(*filePtr)
+	} else {
+		repoURL := os.Args[1]
+		fmt.Println("Git repo URL:", repoURL)
+		processRepo(repoURL)
 	}
-	fmt.Println("Saved in ", repoPath)
+
+}
+
+func readFromFile(filePath string) {
+	fmt.Println("Reading repo file:", filePath)
+
+	file, err := os.Open(filePath)
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start reading from the file with a reader.
+	reader := bufio.NewReader(file)
+
+	var line string
+	for {
+		line, err = reader.ReadString('\n')
+
+		repoLine := strings.Trim(line, "\n")
+		processRepo(repoLine)
+
+		if err != nil {
+			break
+		}
+	}
+}
+
+func processRepo(repoURL string) {
+	repoPath := "./repo-data/" + strings.Split(repoURL, "/")[1]
+	if !strings.Contains(repoURL, "@") {
+		repoPath = "./repo-data/" + strings.Split(repoURL, "/")[4]
+	}
+	fmt.Println("\nProcessing", repoPath)
 
 	repoPathDotGit := repoPath + "/.git"
 	// If repo exists, git pull
@@ -32,29 +70,32 @@ func main() {
 		// Get working directory for the repository
 		worktree, err := repo.Worktree()
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error:", err)
 		}
 
 		// Pull the latest changes from the origin remote and merge into the current branch
 		err = worktree.Pull(&git.PullOptions{RemoteName: "origin"})
 		if err != nil {
-			log.Fatal(err)
+			if err.Error() == "already up-to-date" {
+				log.Println("up to date")
+			} else {
+				log.Fatalln("Error", err)
+			}
 		} else {
-			dt := time.Now()
-			fmt.Println(dt.Format("01-02-2006 15:04:05"), "pull success")
+			log.Println("pull success")
+
 		}
 	} else {
 		// This case is for repo not existing, so we git clone
 		_, err := git.PlainClone(repoPath, false, &git.CloneOptions{
-			URL:      repoULR,
+			URL:      repoURL,
 			Progress: os.Stdout,
 		})
 
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error:", err)
 		} else {
-			dt := time.Now()
-			fmt.Println(dt.Format("01-02-2006 15:04:05"), "clone success")
+			log.Println("clone success")
 		}
 	}
 }
